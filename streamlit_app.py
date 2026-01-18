dcd461ed062da4b870cf34f987528d222274c6bd
Aykut65 <98286703+Aykut65@users.noreply.github.com>
2026-01-18 11:35:55 +0000

    dosya gonderildi

---
+++ streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="BAZ BAGER AI", page_icon="🦅")
+st.markdown("<h1 style='text-align: center; color: #FFD700;'>🦅 BAZ BAGER AI</h1>", unsafe_allow_html=True)
+st.write("### Emret Aykut, asistanın hazır!")
+st.balloons()
